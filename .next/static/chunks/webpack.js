07eb199783de9929abd0570cf009290f47229822
tiagosansopa <tiagosansopa@gmail.com>
2023-05-26 10:45:01 -0600

    updated gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "12059dc6152e8754"; }
+/******/ 		__webpack_require__.h = function() { return "a271e8d9991f894a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.30a206a8913acc8c.hot-update.js
+++ .next/static/webpack/webpack.30a206a8913acc8c.hot-update.js
@@ -23,7 +23,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "649644400a6a9e5d"; }
+/******/ 	__webpack_require__.h = function() { return "a271e8d9991f894a"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
@@ -38,6 +38,11 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 		};
 /******/ 	};
 /******/ }
+/******/ 
+/******/ /* webpack/runtime/nonce */
+/******/ !function() {
+/******/ 	__webpack_require__.nc = undefined;
+/******/ }();
 /******/ 
 /******/ }
 );
